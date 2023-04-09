bbb7b08962ad93e06adafa8bad76a6eed0c1cbb8
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-04-09 06:20:30 +0200

    seperate the ratings collection

---
+++ routes/ratings.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const ratingsController = require('../controllers/ratings');
+const isAuth = require('../middlewares/isAuth');
+
+const router = express.Router();
+
+module.exports = router;
