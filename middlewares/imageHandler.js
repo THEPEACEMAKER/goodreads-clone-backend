d83e4cb5f9fb9d82546eae4f9b7c018f11aa3d54
Mohammed Abdallah <mohammed.a.farhoud.com>
2023-04-15 04:45:09 +0200

    Separate multer into a middlaware

---
+++ middlewares/imageHandler.js
@@ -0,0 +1,26 @@
+const multer = require('multer');
+const path = require('path');
+
+const fileStorage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, path.join(__dirname, '..', 'images'));
+  },
+  filename(req, file, cb) {
+    cb(null, 'image-' + new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/webp'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+module.exports = multer({ storage: fileStorage, fileFilter }).single('image');
